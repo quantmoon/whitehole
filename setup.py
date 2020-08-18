db345f2b71dede8af5ef5c5420f7975a76519435
heligp <20110186@lamolina.edu.pe>
2020-08-18 15:18:49 -0500

    fixed test folders and setup file

--- setup.py
+++ setup.py
@@ -5,15 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="whitehole", 
-    version="0.0.6",
+    version="0.0.2",
     author="QuantMoon Technologies",
     author_email="contacto@quantmoon.com",
     description="Package for read and backtest data of financial assets in the most storage-efficient way, taking care of performance and store using zarr format files",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/quantmoon/whitehole",
+    include_package_data = True,
     packages=setuptools.find_packages(),
-    install_requires=['pandas','numpy','csv','re','xarray','datetime','dateutil'],
+    install_requires=["pandas>=0.22.0",
+                      "numpy>=1.18.2",
+                      "zarr",
+                      "xarray>=0.13.0",
+                      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
